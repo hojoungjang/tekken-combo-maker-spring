187a3034abf2e94f4f8944b98be2978863b011f8
Jang <hjjang501@gmail.com>
2025-03-11 17:35:39 +0900

    Add unit test for ComboService

---
+++ src/test/java/com/github/hojoungjang/tekken_combo_maker/combo/mock/FakeComboService.java
@@ -0,0 +1,27 @@
+package com.github.hojoungjang.tekken_combo_maker.combo.mock;
+
+import com.github.hojoungjang.tekken_combo_maker.combo.controller.IComboService;
+import com.github.hojoungjang.tekken_combo_maker.combo.dto.ComboDto;
+import com.github.hojoungjang.tekken_combo_maker.combo.model.entity.Combo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public class FakeComboService {
+
+//    @Override
+//    public Page<ComboDto> findAllByCharacter(Long characterId, Pageable pageable) {
+//
+//    }
+//
+//    @Override
+//    public Page<ComboDto> findAllByPost(Long postId, Pageable pageable) {
+//
+//    }
+//
+//    @Override
+//    public List<ComboDto> saveAll(List<Combo> combos) {
+//
+//    }
+}
