38144518a30cac154abaaeaadec3db0ded76bae6
Jang <hjjang501@gmail.com>
2025-03-11 11:20:47 +0900

    Add an endpoint to get combo by character in CharacterController

--- src/main/java/com/github/hojoungjang/tekken_combo_maker/character/controller/CharacterController.java
+++ src/main/java/com/github/hojoungjang/tekken_combo_maker/character/controller/CharacterController.java
@@ -1,6 +1,8 @@
 package com.github.hojoungjang.tekken_combo_maker.character.controller;
 
 import com.github.hojoungjang.tekken_combo_maker.character.dto.CharacterDto;
+import com.github.hojoungjang.tekken_combo_maker.combo.controller.IComboService;
+import com.github.hojoungjang.tekken_combo_maker.combo.dto.ComboDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,6 +19,7 @@ import java.util.List;
 public class CharacterController {
 
     private final ICharacterService characterService;
+    private final IComboService comboService;
 
     @GetMapping("/{id}")
     public CharacterDto getById(@PathVariable("id") Long id) {
@@ -26,5 +29,13 @@ public class CharacterController {
     @GetMapping("/")
     public Page<CharacterDto> getAll(Pageable pageable) {
         return characterService.findAll(pageable);
+    }
+
+    @GetMapping("/{id}/combos")
+    public Page<ComboDto> getAllCombos(
+            @PathVariable("id") Long id,
+            Pageable pageable
+    ) {
+        return comboService.findAllByCharacter(id, pageable);
     }
 }
