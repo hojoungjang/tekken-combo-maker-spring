185b7abcb157c9a8b75611117bff963077055d4a
Jang <hjjang501@gmail.com>
2025-03-12 16:16:41 +0900

    Add post response DTO

---
+++ src/main/java/com/github/hojoungjang/tekken_combo_maker/post/dto/PostResponse.java
@@ -0,0 +1,41 @@
+package com.github.hojoungjang.tekken_combo_maker.post.dto;
+
+import com.github.hojoungjang.tekken_combo_maker.member.model.entity.Member;
+import com.github.hojoungjang.tekken_combo_maker.post.model.entity.Post;
+import lombok.Getter;
+
+@Getter
+public class PostResponse {
+
+    record MemberResponse(Long id, String nickName) {}
+
+    private final Long id;
+    private final MemberResponse member;
+    private final String title;
+    private final String content;
+
+    PostResponse(
+            Long id,
+            MemberResponse member,
+            String title,
+            String content
+    ) {
+        this.id = id;
+        this.member = member;
+        this.title = title;
+        this.content = content;
+    }
+
+    public static PostResponse fromEntity(Post post) {
+        MemberResponse member = new MemberResponse(
+                post.getMember().getId(),
+                post.getMember().getNickName()
+        );
+        return new PostResponse(
+                post.getId(),
+                member,
+                post.getTitle(),
+                post.getContent()
+        );
+    }
+}
