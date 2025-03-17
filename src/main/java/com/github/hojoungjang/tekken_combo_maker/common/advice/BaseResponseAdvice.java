d6d6651a4f396b6e9925bf99948371d7ac29f22f
Brian <42661845+hojoungjang@users.noreply.github.com>
2025-03-17 13:06:43 +0900

    공통 API 응답 형식 추가 (#14)
    * Add common success and error response DTO classes
    
    * Use ResponseBodyAdvice to wrap all response into a consistent format

---
+++ src/main/java/com/github/hojoungjang/tekken_combo_maker/common/advice/BaseResponseAdvice.java
@@ -0,0 +1,42 @@
+package com.github.hojoungjang.tekken_combo_maker.common.advice;
+
+import com.github.hojoungjang.tekken_combo_maker.common.dto.BaseErrorResponse;
+import com.github.hojoungjang.tekken_combo_maker.common.dto.BaseResponse;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@RestControllerAdvice
+public class BaseResponseAdvice implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(
+            Object body,
+            MethodParameter returnType,
+            MediaType selectedContentType,
+            Class selectedConverterType,
+            ServerHttpRequest request,
+            ServerHttpResponse response
+    ) {
+        if (body instanceof ProblemDetail) {
+            return BaseErrorResponse.builder()
+                    .success(false)
+                    .error((ProblemDetail) body)
+                    .build();
+        }
+        return BaseResponse.builder()
+                .success(true)
+                .data(body)
+                .build();
+    }
+}
