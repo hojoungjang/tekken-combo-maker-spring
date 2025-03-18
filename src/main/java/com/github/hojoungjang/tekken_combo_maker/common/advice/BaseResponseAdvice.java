c54b267ee618ec4cb4f232c79959181dd4a5136d
Jang <hjjang501@gmail.com>
2025-03-18 15:20:42 +0900

    Remove unused imports

--- src/main/java/com/github/hojoungjang/tekken_combo_maker/common/advice/BaseResponseAdvice.java
+++ src/main/java/com/github/hojoungjang/tekken_combo_maker/common/advice/BaseResponseAdvice.java
@@ -2,14 +2,12 @@ package com.github.hojoungjang.tekken_combo_maker.common.advice;
 
 import com.github.hojoungjang.tekken_combo_maker.common.dto.BaseErrorResponse;
 import com.github.hojoungjang.tekken_combo_maker.common.dto.BaseResponse;
-import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
--- src/main/java/com/github/hojoungjang/tekken_combo_maker/common/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/github/hojoungjang/tekken_combo_maker/common/exception/handler/GlobalExceptionHandler.java
@@ -1,16 +1,12 @@
 package com.github.hojoungjang.tekken_combo_maker.common.exception.handler;
 
 import com.github.hojoungjang.tekken_combo_maker.common.exception.NotFoundException;
-import io.swagger.v3.oas.annotations.Hidden;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.*;
-import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
 @RestControllerAdvice(basePackages = "com.github.hojoungjang.tekken_combo_maker")
