eb4c5e5dbf521ae34f70a2565a0efb9bcccd3698
Jang <hjjang501@gmail.com>
2025-03-22 18:55:03 +0900

    Change audit date fields to Instant type

--- src/main/java/com/github/hojoungjang/tekken_combo_maker/common/model/entity/AuditFieldEntity.java
+++ src/main/java/com/github/hojoungjang/tekken_combo_maker/common/model/entity/AuditFieldEntity.java
@@ -8,7 +8,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
@@ -17,10 +17,10 @@ public class AuditFieldEntity {
 
     @CreatedDate
     @Column(updatable = false)
-    private LocalDateTime createdDate;
+    private Instant createdAt;
 
     @LastModifiedDate
-    private LocalDateTime updatedDate;
+    private Instant updatedAt;
 
 //    @CreatedBy
 //    @Column(updatable = false)
