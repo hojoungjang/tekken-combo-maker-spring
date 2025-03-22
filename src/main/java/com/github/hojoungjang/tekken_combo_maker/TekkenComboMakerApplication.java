8d7a48632a0cb6fc504129284dd3159254df26f1
Jang <hjjang501@gmail.com>
2025-03-22 18:32:11 +0900

    Separate JPA configuration into its own config class

--- src/main/java/com/github/hojoungjang/tekken_combo_maker/TekkenComboMakerApplication.java
+++ src/main/java/com/github/hojoungjang/tekken_combo_maker/TekkenComboMakerApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.util.Optional;
-
 @SpringBootApplication
 @EnableJpaAuditing
 public class TekkenComboMakerApplication {
@@ -13,9 +11,4 @@ public class TekkenComboMakerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TekkenComboMakerApplication.class, args);
 	}
-
-//	@Bean
-//	public AuditorAware<String> auditorProvider() {
-//		return () -> Optional.of(UUID.randomUUID().toString());
-//	}
 }
