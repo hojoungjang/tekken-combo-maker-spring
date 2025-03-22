6e196eac32a1a9c62db047caf842bd29a7ab6d7c
Jang <hjjang501@gmail.com>
2025-03-22 18:54:12 +0900

    Remove duplicate

--- src/main/java/com/github/hojoungjang/tekken_combo_maker/TekkenComboMakerApplication.java
+++ src/main/java/com/github/hojoungjang/tekken_combo_maker/TekkenComboMakerApplication.java
@@ -2,10 +2,8 @@ package com.github.hojoungjang.tekken_combo_maker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class TekkenComboMakerApplication {
 
 	public static void main(String[] args) {
