02fef613f76f8ddfb6119cecfb4a177ef9f81c61
Jang <hjjang501@gmail.com>
2025-03-27 23:12:15 +0900

    Add AuthenticationSuccessHandler and AuthenticationFailureHandler implementations

---
+++ src/main/java/com/github/hojoungjang/tekken_combo_maker/auth/LoginFailureHandler.java
@@ -0,0 +1,43 @@
+package com.github.hojoungjang.tekken_combo_maker.auth;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.github.hojoungjang.tekken_combo_maker.common.dto.BaseErrorResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ProblemDetail;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Slf4j
+public class LoginFailureHandler implements AuthenticationFailureHandler {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        log.error(String.format("Authentication Failed: %s", exception.getMessage()));
+
+        ProblemDetail errorBody = ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, exception.getMessage());
+        BaseErrorResponse responseBody = BaseErrorResponse.builder()
+                .success(false)
+                .error(errorBody)
+                .build();
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        ObjectWriter objectWriter = objectMapper.writer().withDefaultPrettyPrinter();
+        PrintWriter writer = response.getWriter();
+        writer.write(objectWriter.writeValueAsString(responseBody));
+    }
+}
