16beeeccb1d77b61299ea7b06d8f659ba6723e64
Jang <hjjang501@gmail.com>
2025-03-27 23:22:45 +0900

    Configure spring security configuration class
    * Disable CSRF protection temporarily
    * Add AuthorizationFilter via authorizeHttpRequests' requestMatchers configuration
    * Disable HTTP basic auth
    * Enable form-login auth and customize to fit REST style (JSON response) using custom success/fail handlers
    * Configure custom AuthenticationEntryPoint to handle unauthorized errors in REST style (JSON response)
    * Configure custom AuthenticationManager to use custom UserDetailsService and BCrypt PasswordEncoder

---
+++ src/main/java/com/github/hojoungjang/tekken_combo_maker/auth/SecurityConfig.java
@@ -0,0 +1,99 @@
+package com.github.hojoungjang.tekken_combo_maker.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+@Configuration
+public class SecurityConfig {
+
+    // TODO: Remove in production
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring()
+                .requestMatchers(toH2Console());
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(
+            HttpSecurity http,
+            AuthenticationEntryPoint restLoginAuthenticationEntryPoint,
+            AuthenticationSuccessHandler loginSuccessHandler,
+            AuthenticationFailureHandler loginFailureHandler
+    ) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);      // TODO: 제대로 설정해주기
+
+        http.authorizeHttpRequests(c -> c
+                        .requestMatchers(HttpMethod.POST, "/api/v1/members").permitAll()
+                        .anyRequest().authenticated());
+
+        http.httpBasic(HttpBasicConfigurer::disable);
+
+        http.formLogin(c -> c
+                        .successHandler(loginSuccessHandler)
+                        .failureHandler(loginFailureHandler)
+                        .loginProcessingUrl("/api/v1/auth/login"));
+
+        http.exceptionHandling(c -> c
+                .authenticationEntryPoint(restLoginAuthenticationEntryPoint));    // TODO: defaultAuthenticationEntryPointFor() 사용 확인하기
+
+        // TODO: Add session management configuration
+        // http.sessionManagement()
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            UserDetailsService userDetailsServiceImpl,
+            PasswordEncoder bcryptPasswordEncoder
+    ) {
+        // Username & password authentication provider
+        DaoAuthenticationProvider daoAuthProvider = new DaoAuthenticationProvider();
+        daoAuthProvider.setUserDetailsService(userDetailsServiceImpl);
+        daoAuthProvider.setPasswordEncoder(bcryptPasswordEncoder);
+
+        return new ProviderManager(daoAuthProvider);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsServiceImpl() {
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    public PasswordEncoder bcryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationEntryPoint restLoginAuthenticationEntryPoint() {
+        return new RestLoginAuthenticationEntryPoint();
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler loginSuccessHandler() {
+        return new LoginSuccessHandler();
+    }
+
+    @Bean
+    public AuthenticationFailureHandler loginFailureHandler() {
+        return new LoginFailureHandler();
+    }
+}
