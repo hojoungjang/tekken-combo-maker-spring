c73df3282ce330da6e448feff2d4136546e0ee38
Jang <hjjang501@gmail.com>
2025-03-31 17:12:57 +0900

    Raise exception if oauth user attempts to login with password

---
+++ src/main/java/com/github/hojoungjang/tekken_combo_maker/common/exception/InvalidLoginMethodException.java
@@ -0,0 +1,7 @@
+package com.github.hojoungjang.tekken_combo_maker.common.exception;
+
+public class InvalidLoginMethodException extends RuntimeException {
+    public InvalidLoginMethodException(String message) {
+        super(message);
+    }
+}
