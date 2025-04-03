5c5604658395560bed49d4d8f78df0e094ebcdfd
Jang <hjjang501@gmail.com>
2025-04-03 16:55:12 +0900

    Clarify name of heat engager attribute

--- src/main/java/com/github/hojoungjang/tekken_combo_maker/move/model/enums/MoveAttribute.java
+++ src/main/java/com/github/hojoungjang/tekken_combo_maker/move/model/enums/MoveAttribute.java
@@ -7,6 +7,6 @@ public enum MoveAttribute {
     POWER_CRUSH,        // 파워 크러시
     HOMING,             // 호밍기
     TORNADO,            // 토내이도
-    HEAT,               // 히트 인게이져
+    HEAT_ENGAGER,       // 히트 인게이져
     RECOVERY_DELETE     // 리커버리 게이지 소멸
 }
