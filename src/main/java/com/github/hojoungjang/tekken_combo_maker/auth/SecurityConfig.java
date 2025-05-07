43a44c7687d0450bd733f8c26245d5c885eddb67
Brian <42661845+hojoungjang@users.noreply.github.com>
2025-05-07 14:40:19 +0900

    웹 프론트엔드 애플리케이션용 CORS 설정 추가 (#25)
    * Add Spring Security CORS settings for web frontend application
    
    * Remove unused log code

--- src/main/java/com/github/hojoungjang/tekken_combo_maker/auth/SecurityConfig.java
+++ src/main/java/com/github/hojoungjang/tekken_combo_maker/auth/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.github.hojoungjang.tekken_combo_maker.auth;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.hojoungjang.tekken_combo_maker.auth.oauth2.CustomOAuth2UserService;
 import com.github.hojoungjang.tekken_combo_maker.member.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -20,17 +21,34 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 @Configuration
 public class SecurityConfig {
 
+    @Value("${WEB_CLIENT_APP_URL}")
+    private String webClientOrigin;
+
     // TODO: Remove in production
     @Bean
     public WebSecurityCustomizer configure() {
         return (web) -> web.ignoring()
                 .requestMatchers(toH2Console());
+    }
+
+    @Bean
+    UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(webClientOrigin));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "PATCH", "DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
@@ -47,6 +65,7 @@ public class SecurityConfig {
                 .requestMatchers("/swagger", "/swagger-ui/**", "/api-docs/**", "/v3/api-docs/**").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/v1/members").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/v1/characters/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/v1/moves").permitAll()
                 .requestMatchers("/img/character/**").permitAll()
                 .anyRequest().authenticated());
 
