4e3701deddb567b0faceb1a4f192da7ac64779d4
Jang <hjjang501@gmail.com>
2025-05-13 15:59:52 +0900

    Add label field to Character entity

--- src/main/java/com/github/hojoungjang/tekken_combo_maker/character/dto/CharacterResponse.java
+++ src/main/java/com/github/hojoungjang/tekken_combo_maker/character/dto/CharacterResponse.java
@@ -8,14 +8,16 @@ public class CharacterResponse {
     private final Long id;
     private final String name;
     private final String fullName;
+    private final String label;
     private final String description;
     private final String avatarImageName;
 
     @Builder
-    CharacterResponse(Long id, String name, String fullName, String description, String avatarImageName) {
+    CharacterResponse(Long id, String name, String fullName, String label, String description, String avatarImageName) {
         this.id = id;
         this.name = name;
         this.fullName = fullName;
+        this.label = label;
         this.description = description;
         this.avatarImageName = avatarImageName;
     }
