0e37e25f78f1015119f59b869ad6440b60a8dbdf
Jang <hjjang501@gmail.com>
2025-05-23 18:21:55 +0900

    필터 설정 테스트 환경에서 제거

--- src/main/java/com/github/hojoungjang/tekken_combo_maker/common/config/FilterConfig.java
+++ src/main/java/com/github/hojoungjang/tekken_combo_maker/common/config/FilterConfig.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("!test")
 public class FilterConfig {
 
     @Value("${application.env.rest-client-auth-key}")
