87ab06ed2d42f845639fb16b5ed61df307bae2eb
Jang <hjjang501@gmail.com>
2025-05-24 21:09:38 +0900

    FilterConfig 을 prod 프로필에서만 활성화

--- src/main/java/com/github/hojoungjang/tekken_combo_maker/common/config/FilterConfig.java
+++ src/main/java/com/github/hojoungjang/tekken_combo_maker/common/config/FilterConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("!test")
+@Profile("prod")
 public class FilterConfig {
 
     @Value("${application.env.rest-client-auth-key}")
